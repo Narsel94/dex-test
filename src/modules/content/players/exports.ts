ab614704562ff9f5f85d81ab69f2d7601f6facc5
Narsel94 <potapserg874@gmail.com>
2023-11-12 02:45:02 +0200

    pages

--- src/modules/content/players/exports.ts
+++ src/modules/content/players/exports.ts
@@ -1,7 +1,6 @@
 import { AddNewPlayer } from "../../../pages/add-new-player/add-new-player";
 import { PlayersList } from "../../../pages/players-list/players-list";
 import { PlayerInfo } from "./player-info/player-info";
-import { UpdatePlayer } from "../../../pages/update-player/update-player";
 import { PlayerForm } from "./update-player-form/update-player-form";
 
-export {AddNewPlayer, PlayersList, PlayerInfo, UpdatePlayer, PlayerForm}
+export {AddNewPlayer, PlayersList, PlayerInfo,  PlayerForm}
--- src/modules/content/teams/exports.ts
+++ src/modules/content/teams/exports.ts
@@ -1,4 +1,3 @@
-import { TeamsList } from "../../../pages/teams-list/teams-list";
 import { AddNewTeam } from "../../../pages/add-new-team/add-new-team";
 import { TeamInfo } from "./team-info/team-info";
 import { ErrroElement } from "./components/error-element/error-element";
@@ -6,4 +5,4 @@ import { TeamCard } from "./components/team-card/team-card";
 import { Roster } from "./components/roster/roster";
 
 
-export {TeamsList, AddNewTeam, TeamInfo as SingleTeam, ErrroElement, TeamCard, Roster}
+export { AddNewTeam, TeamInfo as SingleTeam, ErrroElement, TeamCard, Roster}
